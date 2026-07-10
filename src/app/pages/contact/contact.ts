8e3563ad8a2709aa3ec1cc07c189e55376294fd9
Roberth Patiño <rendpc68@gmail.com>
2026-07-10 15:38:51 -0500

    feat: agregar validaciones al formulario de contacto

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,9 +1,48 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
-export class Contact {}
+export class Contact {
+
+  nombre = '';
+  correo = '';
+  mensaje = '';
+
+  enviado = false;
+
+  enviarFormulario() {
+
+    this.enviado = true;
+
+    if (
+      this.nombre.trim().length >= 3 &&
+      this.validarCorreo(this.correo) &&
+      this.mensaje.trim().length >= 10
+    ) {
+
+      alert('¡Mensaje enviado correctamente!');
+
+      this.nombre = '';
+      this.correo = '';
+      this.mensaje = '';
+      this.enviado = false;
+
+    }
+
+  }
+
+  validarCorreo(correo: string): boolean {
+
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    return regex.test(correo);
+
+  }
+
+}
